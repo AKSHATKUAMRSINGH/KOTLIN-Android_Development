a5bafb8a5f172cce17ac3c67beeab690f9010027
AKSHAT KUMAR SINGH <136428913+AKSHATKUAMRSINGH@users.noreply.github.com>
2025-06-19 00:10:57 +0530

    Add files via upload

---
+++ val_var_boolean.kt
@@ -0,0 +1,6 @@
+fun main (){
+    val x: Boolean=true
+    val y: Boolean=false
+    println(x)
+    println(y)
+}
---
+++ val_var_float.kt
@@ -0,0 +1,6 @@
+fun main (){
+    val x=5f
+    val y=6.234f
+    println(x)
+    println(y)
+}
